a62ddcbe571342f70b0a3f1e30ff2827b341be98
CeeDeeBee <cdbarnes98@gmail.com>
2020-02-17 23:09:21 -0500

    Added timer and new quarter button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,13 +7,36 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [clock, setClock] = useState({ minutes: '15', seconds: '00' });
+  const [quarter, setQuarter] = useState(1);
+
+  useEffect(() => {
+    const clockTimeout = setTimeout(() => {
+      if (clock.minutes === '00' && clock.seconds === '00') {
+        // increase quarter
+      } else if (clock.seconds === '00') {
+        setClock({
+          minutes: clock.minutes <= '10' ? `0${clock.minutes - 1}` : clock.minutes - 1,
+          seconds: '59'
+        });
+      } else {
+        setClock({
+          minutes: clock.minutes,
+          seconds: clock.seconds <= '10' ? `0${clock.seconds - 1}` : clock.seconds - 1
+        });
+      }
+    }, 1000);
+
+    return () => clearTimeout(clockTimeout);
+  }, [clock.minutes, clock.seconds]);
 
   const updateScore = (team, ammount) => {
-    if (team === 'home') {
-      setHomeScore(homeScore + ammount);
-    } else {
-      setAwayScore(awayScore + ammount);
-    }
+    team === 'home' ? setHomeScore(homeScore + ammount) : setAwayScore(awayScore + ammount);
+  }
+
+  const incrementQuarter = () => {
+    setClock({ minutes: '15', seconds: '00' });
+    quarter < 4 ? setQuarter(quarter + 1) : setQuarter(1);
   }
 
   return (
@@ -27,13 +50,13 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{clock.minutes}:{clock.seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -45,6 +68,7 @@ function App() {
           <button className="awayButtons__touchdown" onClick={() => updateScore('away', 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => updateScore('away', 3)}>Away Field Goal</button>
         </div>
+        <button className="incrementQuarterButton" onClick={incrementQuarter}>New Quarter</button>
       </section>
     </div>
   );
